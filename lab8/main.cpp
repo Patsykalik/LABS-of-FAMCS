e93941e1b2cbda1ff94d63eb349c4fc95f1cd41f
Patsykalik <apacykalik@gmail.com>
2025-12-21 17:14:10 +0300

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
+#include "Calculator.h"
 
+int main() {
+    Calculator calc;
+    std::cout << "Result = " << calc.evaluate("11 * 13 - 113 / (13 + 43)") << std::endl;
+    return 0;
+}
